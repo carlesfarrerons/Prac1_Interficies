5307f528b757f08258f5ec3821eab1943a6235df
carlesfarrerons <carles.farrerons@mataro.epiaedu.cat>
2016-10-20 18:44:17 +0200

    Add project files.

---
+++ Prac1_Interficies/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+    Dim posicioInicialX
+    Dim posicioInicialY
+    Dim posicioFinalX
+    Dim posicioFinalY
+    Dim ratoli
+    Private Sub Form1_DoubleClick(sender As Object, e As EventArgs) Handles MyBase.DoubleClick
+
+        If Me.WindowState = FormWindowState.Maximized Then
+            Me.WindowState = FormWindowState.Normal
+        Else
+            Me.WindowState = FormWindowState.Maximized
+        End If
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+        If e.KeyCode = Keys.Escape Then
+            Me.Text = " "
+            e.SuppressKeyPress = True
+
+        ElseIf e.KeyCode = Keys.I Or e.KeyCode = Keys.L Then
+
+            Me.Text = StrReverse(Me.Text)
+            e.SuppressKeyPress = True
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+
+        Me.Text = Me.Text + e.KeyChar
+
+    End Sub
+    Private Sub Form1_SizeChanged(sender As Object, e As EventArgs) Handles MyBase.SizeChanged
+        Me.Text = "altura: " + CStr(Size.Height) + " amplada: " + CStr(Size.Width)
+
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+
+        posicioInicialX = e.X
+        posicioInicialY = e.Y
+
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+
+        posicioFinalX = e.X
+        posicioFinalY = e.Y
+        If (e.Button = MouseButtons.Right) Then
+            ratoli = "   DRETA"
+        Else
+            ratoli = "   ESQUERRA"
+        End If
+        Me.Text = "Inici X: " + CStr(posicioInicialX) + " Inici Y: " + CStr(posicioInicialY) + " Final X: " + CStr(posicioFinalX) + "Final Y: " + CStr(posicioFinalY) + ratoli
+
+    End Sub
+End Class
---
+++ Prac1_Interficies/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Prac1_Interficies")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Prac1_Interficies")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6d60ea49-7189-48a4-bf78-01be50888bd6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
